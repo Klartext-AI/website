2c497a0edf178815228aeb5fb094c58121f8893d
Felix Krause <felix_krause1999@yahoo.de>
2025-11-22 15:57:13 +0100

    updating link to proper klartext domain with HTTPS

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'http://klartext-ai.com',
+  site: 'https://klartext-ai.com',
   base: '/',
   i18n: {
     defaultLocale: 'de',
